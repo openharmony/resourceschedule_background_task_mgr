c3fc7154ff6bafcfae447db53a79c89e2a20961f
zhangxin_T <zhangxin312@huawei.com>
2022-08-22 10:40:13 +0800

    tdd bugfix
    Signed-off-by: zhangxin_T <zhangxin312@huawei.com>

---
+++ interfaces/test/unittest/bgtask_manager_client_test/bgtask_client_unit_test.cpp
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "gtest/gtest.h"
+
+#include "bgtaskmgr_inner_errors.h"
+#include "background_task_mgr_helper.h"
+#include "event_type.h"
+
+using namespace testing::ext;
+
+namespace OHOS {
+namespace BackgroundTaskMgr {
+class BgTaskManagerClientTest : public testing::Test {
+public:
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+    void SetUp();
+    void TearDown();
+};
+
+void BgTaskManagerClientTest::SetUpTestCase() {}
+
+void BgTaskManagerClientTest::TearDownTestCase() {}
+
+void BgTaskManagerClientTest::SetUp() {}
+
+void BgTaskManagerClientTest::TearDown() {}
+
+/**
+ * @tc.name: ReportStateChangeEvent_001
+ * @tc.desc: report some state change infos to bgtask service api test.
+ * @tc.type: FUNC
+ * @tc.require: SR000H0G4B AR000H2MSF AR000H2MSG AR000H2MSI AR000H2MSJ AR000H2MSH
+ */
+HWTEST_F(BgTaskManagerClientTest, ReportStateChangeEvent_001, TestSize.Level1)
+{
+    EXPECT_EQ((int32_t)BackgroundTaskMgrHelper::ReportStateChangeEvent(
+        EventType::DIS_COMP_CHANGE, "test"), (int32_t)ERR_OK);
+}
+}
+}
