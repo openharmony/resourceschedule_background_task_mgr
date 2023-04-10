08f80b7198061f6a6da53c4dff2b036fef87ddf5
ywx1057076 <yanpei12@huawei.com>
2023-04-10 14:10:04 +0800

    fix：basic
    Signed-off-by: ywx1057076 <yanpei12@huawei.com>

--- interfaces/kits/js/@ohos.resourceschedule.backgroundTaskManager.d.ts
+++ interfaces/kits/js/@ohos.resourceschedule.backgroundTaskManager.d.ts
@@ -13,7 +13,7 @@
  * limitations under the License.
  */
 
-import { AsyncCallback , Callback} from './basic';
+import { AsyncCallback , Callback} from './@ohos.base';
 import { WantAgent } from "./@ohos.wantAgent";
 import Context from './application/BaseContext';
 
