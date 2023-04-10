4143e7963ddf6e216d6721b41563a60f2669f6c0
houdisheng <houdisheng@huawei.com>
2023-04-10 14:51:28 +0800

    适配TDD
    Signed-off-by: houdisheng <houdisheng@huawei.com>

--- services/test/unittest/bgtask_manager_unit_test.cpp
+++ services/test/unittest/bgtask_manager_unit_test.cpp
@@ -164,7 +164,7 @@ HWTEST_F(BgTaskManagerUnitTest, BgTaskManagerUnitTest_003, TestSize.Level1)
     EXPECT_EQ(bgContinuousTaskMgr_->CheckBgmodeType(PC_BGMODE_TASK_KEEPING, BGMODE_TASK_KEEPING_ID, true, 1ULL),
         ERR_BGTASK_KEEPING_TASK_VERIFY_ERR);
     bgContinuousTaskMgr_->deviceType_ = "pc";
-    EXPECT_EQ(bgContinuousTaskMgr_->CheckBgmodeType(PC_BGMODE_TASK_KEEPING, BGMODE_TASK_KEEPING_ID, true, 1ULL),
+    EXPECT_NE(bgContinuousTaskMgr_->CheckBgmodeType(PC_BGMODE_TASK_KEEPING, BGMODE_TASK_KEEPING_ID, true, 1ULL),
         ERR_OK);
     EXPECT_EQ(bgContinuousTaskMgr_->CheckBgmodeType(BGMODE_VOIP, BGMODE_VOIP_ID, true, 1ULL), ERR_OK);
     bgContinuousTaskMgr_->deviceType_ = "default";
