a4d5989117cc38f3b43be47dec626468966f2c5d
朱天怡 <zhutianyi2@huawei.com>
2023-04-18 06:12:50 +0000

    bugfix
    Signed-off-by: 朱天怡 <zhutianyi2@huawei.com>

--- services/transient_task/src/input_manager.cpp
+++ services/transient_task/src/input_manager.cpp
@@ -24,7 +24,7 @@ namespace BackgroundTaskMgr {
 
 InputManager::InputManager(const std::shared_ptr<AppExecFwk::EventRunner>& runner)
 {
-    if (runner == nullptr) {
+    if (runner != nullptr) {
         SetEventRunner(runner);
     }
 }
