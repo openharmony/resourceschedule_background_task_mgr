a1dc143ea125c2dfa6778c9d1918fb2f157fb747
Zhao-PengFei35 <zhaopengfei35@huawei.com>
2023-08-21 13:15:06 +0800

    fix bug
    Signed-off-by: Zhao-PengFei35 <zhaopengfei35@huawei.com>
    Change-Id: I29bb14ca1db8a8f0c0e250a99731313dbf2cda5d

--- services/test/unittest/mock/include/ibundle_manager_helper_mock.h
+++ services/test/unittest/mock/include/ibundle_manager_helper_mock.h
@@ -24,7 +24,7 @@ namespace BackgroundTaskMgr {
 class MockBundleManagerHelper final : public IBundleManagerHelper
 {
 public:
-    ~IBundleManagerHelper() override = default;
+    ~MockBundleManagerHelper() override = default;
 
     MOCK_METHOD4(GetApplicationInfo, bool(const std::string &, const AppExecFwk::ApplicationFlag,
         const int, AppExecFwk::ApplicationInfo &));
