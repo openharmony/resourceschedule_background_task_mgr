5f5c347a65473655b934a05b4f21e36855d4f933
lipengfei <lipengfei53@huawei.com>
2024-05-18 15:41:14 +0000

    update services/continuous_task/src/bg_continuous_task_mgr.cpp.
    Signed-off-by: lipengfei <lipengfei53@huawei.com>

--- services/continuous_task/src/bg_continuous_task_mgr.cpp
+++ services/continuous_task/src/bg_continuous_task_mgr.cpp
@@ -580,7 +580,7 @@ ErrCode BgContinuousTaskMgr::StartBackgroundRunningForInner(const sptr<Continuou
 {
     ErrCode result = ERR_OK;
     int32_t uid = taskParam->uid_;
-    pid_t callingPid = IPCSkeleton::GetCallingPid(); 
+    pid_t callingPid = IPCSkeleton::GetCallingPid();
     uint64_t fullTokenId = ((uid == VOIP_SA_UID) ? taskParam->tokenId_ : IPCSkeleton::GetCallingFullTokenID());
     std::string bundleName = BundleManagerHelper::GetInstance()->GetClientBundleName(uid);
     std::string abilityName = "Webview" + std::to_string(taskParam->bgModeId_);
