de729c0d8ebaef61b91a22721064877937fd3c64
胡伟 <huwei169@huawei.com>
2025-02-14 02:49:40 +0000

    update services/test/unittest/bgtask_misc_unit_test.cpp.
    Signed-off-by: 胡伟 <huwei169@huawei.com>

--- services/test/unittest/bgtask_misc_unit_test.cpp
+++ services/test/unittest/bgtask_misc_unit_test.cpp
@@ -116,8 +116,7 @@ HWTEST_F(BgTaskMiscUnitTest, AppStateObserverTest_001, TestSize.Level1)
     appStateObserver->OnAppStopped(appStateData);
     appStateData.state = static_cast<int32_t>(AppExecFwk::ApplicationState::APP_STATE_END);
     appStateObserver->OnAppStopped(appStateData);
-    appStateObserver->OnAppStopped(appStateData);
-    EXPECT_EQ(appStateData.uid, 1);
+    EXPECT_TRUE(appStateObserver->ValidateAppStateData(appStateData));
 }
 
 /**
