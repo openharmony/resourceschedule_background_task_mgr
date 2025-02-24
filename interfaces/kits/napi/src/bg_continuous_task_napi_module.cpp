9ca1eee21582f1096f8343705d763dabae08f2c3
胡伟 <huwei169@huawei.com>
2025-02-24 03:32:32 +0000

    update interfaces/kits/napi/src/bg_continuous_task_napi_module.cpp.
    Signed-off-by: 胡伟 <huwei169@huawei.com>

--- interfaces/kits/napi/src/bg_continuous_task_napi_module.cpp
+++ interfaces/kits/napi/src/bg_continuous_task_napi_module.cpp
@@ -819,8 +819,8 @@ bool CheckOnParam(napi_env env, uint32_t argc, napi_value argv[], int size)
         BGTASK_LOGE("type must be string");
         return false;
     }
-    if (type != "onContinuousTaskCancel") {
-        BGTASK_LOGE("type must be onContinuousTaskCancel");
+    if (type != "continuousTaskCancel") {
+        BGTASK_LOGE("type must be continuousTaskCancel");
         return false;
     }
     // arg[1] : callback
@@ -874,8 +874,8 @@ bool CheckOffParam(napi_env env, uint32_t argc, napi_value argv[], int size)
         BGTASK_LOGE("type must be string");
         return false;
     }
-    if (type != "onContinuousTaskCancel") {
-        BGTASK_LOGE("type must be onContinuousTaskCancel");
+    if (type != "continuousTaskCancel") {
+        BGTASK_LOGE("type must be continuousTaskCancel");
         return false;
     }
     // arg[1] : callback
