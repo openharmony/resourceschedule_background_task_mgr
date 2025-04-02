22205e248afc119a7ffca401390f3d3de1f43751
zhushuanghong <zhushuanghong5@huawei.com>
2025-04-02 15:57:35 +0800

    支持FDSAN检测能力
    Signed-off-by: zhushuanghong <zhushuanghong5@huawei.com>

--- services/continuous_task/src/bg_continuous_task_mgr.cpp
+++ services/continuous_task/src/bg_continuous_task_mgr.cpp
@@ -145,16 +145,16 @@ void BgContinuousTaskMgr::ReclaimProcessMemory(int32_t pid)
     std::string contentStr = "1";
     FILE *file = fopen(path.c_str(), "w");
     if (file == nullptr) {
-        BGTASK_LOGE("Fail to open file: %{private}s, erron: %{public}s", path.c_str(), strerror(erron));
+        BGTASK_LOGE("Fail to open file: %{private}s, errno: %{public}s", path.c_str(), strerror(errno));
         return;
     }
     size_t res = fwrite(contentStr.c_str(), 1, contentStr.length(), file);
     if (res != contentStr.length()) {
-        BGTASK_LOGE("Fail to write file: %{private}s, erron: %{public}s", path.c_str(), strerror(erron));
+        BGTASK_LOGE("Fail to write file: %{private}s, errno: %{public}s", path.c_str(), strerror(errno));
     }
     int closeResult = fclose(file);
     if (closeResult < 0) {
-        BGTASK_LOGE("Fail to close file: %{private}s, erron: %{public}s", path.c_str(), strerror(erron));
+        BGTASK_LOGE("Fail to close file: %{private}s, errno: %{public}s", path.c_str(), strerror(errno));
     }
     BGTASK_LOGI("BgContinuousTaskMgr reclaimProcessMemory pid: %{public}d end.", pid);
 }
