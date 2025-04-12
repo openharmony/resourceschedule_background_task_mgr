b84236849e59e9014118e4580df3588e7e1fc78e
zhushuanghong <zhushuanghong5@huawei.com>
2025-04-12 09:52:59 +0800

    后台任务申请失败修复
    Signed-off-by: zhushuanghong <zhushuanghong5@huawei.com>

--- services/common/src/data_storage_helper.cpp
+++ services/common/src/data_storage_helper.cpp
@@ -183,11 +183,6 @@ bool DataStorageHelper::CreateNodeFile(const std::string &filePath)
         BGTASK_LOGD("the file: %{private}s already exists.", filePath.c_str());
         return true;
     }
-    char resolvedPath[PATH_MAX] = {0};
-    if (realpath(filePath.c_str(), resolvedPath) == nullptr) {
-        BGTASK_LOGE("Fail to resolved file: %{private}s, errno: %{public}s", filePath.c_str(), strerror(errno));
-        return false;
-    }
     FILE *file = fopen(filePath.c_str(), "w+");
     if (file == nullptr) {
         BGTASK_LOGE("Fail to open file: %{private}s, errno: %{public}s", filePath.c_str(), strerror(errno));
