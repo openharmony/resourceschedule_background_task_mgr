d0620731bed6ac44deea0dc432795fc53b44dd16
zhushuanghong <zhushuanghong5@huawei.com>
2025-07-10 22:23:47 +0800

    未注册暂停态时，取消长时任务
    Signed-off-by: zhushuanghong <zhushuanghong5@huawei.com>

--- services/continuous_task/src/bg_continuous_task_mgr.cpp
+++ services/continuous_task/src/bg_continuous_task_mgr.cpp
@@ -804,16 +804,13 @@ ErrCode BgContinuousTaskMgr::UpdateBackgroundRunningInner(const std::string &tas
     const sptr<ContinuousTaskParam> &taskParam)
 {
     ErrCode ret;
-
     auto iter = continuousTaskInfosMap_.find(taskInfoMapKey);
     if (iter == continuousTaskInfosMap_.end()) {
         BGTASK_LOGW("continuous task is not exist: %{public}s, use start befor update", taskInfoMapKey.c_str());
         return ERR_BGTASK_OBJECT_NOT_EXIST;
     }
-
     auto continuousTaskRecord = iter->second;
     auto oldModes = continuousTaskRecord->bgModeIds_;
-
     BGTASK_LOGI("continuous task mode %{public}d, old modes: %{public}s, new modes %{public}s, isBatchApi %{public}d,"
         " abilityId %{public}d", continuousTaskRecord->bgModeId_,
         continuousTaskRecord->ToString(continuousTaskRecord->bgModeIds_).c_str(),
