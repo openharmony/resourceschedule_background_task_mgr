d14d3b841f13161d648d44ddf661cabe5a470643
胡伟 <huwei169@huawei.com>
2025-07-30 08:17:01 +0000

    update services/continuous_task/include/continuous_task_record.h.
    Signed-off-by: 胡伟 <huwei169@huawei.com>

--- services/continuous_task/include/continuous_task_record.h
+++ services/continuous_task/include/continuous_task_record.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2022-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -80,7 +80,6 @@ private:
     std::shared_ptr<WantAgentInfo> wantAgentInfo_ {nullptr};
     std::string appName_ {""};
     uint64_t fullTokenId_ {0};
-    uint64_t callingTokenId_ {0};
     bool isBatchApi_ {false};
     std::vector<uint32_t> bgModeIds_ {};
     std::vector<uint32_t> bgSubModeIds_ {};
