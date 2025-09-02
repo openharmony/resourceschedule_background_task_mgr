86a0cb7c9063d3af34ba0c10473bcee7f19dfe5d
胡伟 <huwei169@huawei.com>
2025-09-02 06:54:21 +0000

    update
    Signed-off-by: 胡伟 <huwei169@huawei.com>

--- services/test/unittest/bgtask_manager_abnormal_unit_test/bgtask_manager_abnormal_unit_test.cpp
+++ services/test/unittest/bgtask_manager_abnormal_unit_test/bgtask_manager_abnormal_unit_test.cpp
@@ -144,7 +144,9 @@ HWTEST_F(BgTaskManagerAbnormalUnitTest, BackgroundTaskMgrServiceAbnormalTest_005
     EXPECT_EQ(BackgroundTaskMgrService_->UpdateBackgroundRunning(
         taskParam, notificationId, continuousTaskId), ERR_BGTASK_SYS_NOT_READY);
     int32_t abilityId = -1;
-    EXPECT_EQ(BackgroundTaskMgrService_->StopBackgroundRunning("test", nullptr, abilityId), ERR_BGTASK_SYS_NOT_READY);
+    int32_t continuousTaskId = -1;
+    EXPECT_EQ(BackgroundTaskMgrService_->StopBackgroundRunning("test", nullptr, abilityId, continuousTaskId),
+        ERR_BGTASK_SYS_NOT_READY);
 }
 
 /**
