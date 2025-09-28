f09d543d4a055b8af475f34dbde569edc37217d4
cheng-shichang <chengshichang@huawei.com>
2025-09-28 16:03:59 +0800

    Signed-off-by: csc<chengshichang@huawei.com>

--- interfaces/kits/napi/src/bg_continuous_task_napi_module.cpp
+++ interfaces/kits/napi/src/bg_continuous_task_napi_module.cpp
@@ -648,13 +648,13 @@ bool StartBackgroundRunningCheckModes(napi_env env, bool isThrow, AsyncCallbackI
     }
     int32_t specialModeSize = std::count(backgroundTaskModes.begin(), backgroundTaskModes.end(),
         BackgroundTaskMode::MODE_SPECIAL_SCENARIO_PROCESSING);
-    // 特殊场景处理类型长时任务单词最多允许申请一个
+    // 特殊场景处理长时任务类型单次最多允许申请一个
     if (specialModeSize > MAX_TASK_NUMS) {
         Common::HandleErrCode(env, ERR_BGTASK_SPECIAL_SCENARIO_PROCESSING_ONLY_ALLOW_ONE_APPLICATION, true);
         asyncCallbackInfo->errCode = ERR_BGTASK_SPECIAL_SCENARIO_PROCESSING_ONLY_ALLOW_ONE_APPLICATION;
         return false;
     }
-    // 特殊场景处理类型长时任务与其他类型长时任务互斥
+    // 特殊场景处理长时任务类型与其他长时任务类型互斥
     if (specialModeSize == MAX_TASK_NUMS && backgroundTaskModes.size() > MAX_TASK_NUMS) {
         Common::HandleErrCode(env, ERR_BGTASK_SPECIAL_SCENARIO_PROCESSING_CONFLICTS_WITH_OTHER_TASK, true);
         asyncCallbackInfo->errCode = ERR_BGTASK_SPECIAL_SCENARIO_PROCESSING_CONFLICTS_WITH_OTHER_TASK;
