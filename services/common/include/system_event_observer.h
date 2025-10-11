aa5672875aee0c20dda60086264ba7eb971f9a9a
beauty-bag <huwei169@huawei.com>
2025-10-11 09:17:47 +0800

    update: 更新文件 system_event_observer.h
    Signed-off-by: beauty-bag <huwei169@huawei.com>

--- services/common/include/system_event_observer.h
+++ services/common/include/system_event_observer.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2022-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -38,6 +38,9 @@ public:
 private:
     void OnReceiveEventContinuousTask(const EventFwk::CommonEventData &eventData);
     void OnReceiveEventEfficiencyRes(const EventFwk::CommonEventData &eventData);
+    void OnBannerNotificationActionButtonClick(const std::shared_ptr<AppExecFwk::EventHandler> &handler,
+        const std::shared_ptr<BgContinuousTaskMgr> &bgContinuousTaskMgr,
+        const EventFwk::CommonEventData &eventData);
 
 private:
     std::weak_ptr<AppExecFwk::EventHandler> handler_;
