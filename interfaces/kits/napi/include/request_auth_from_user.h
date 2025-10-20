26d39a4afb1519ac157bbb8bcba3be26d361ff8a
beauty-bag <huwei169@huawei.com>
2025-10-20 20:29:18 +0800

    new: 新建文件 request_auth_from_user.h
    Signed-off-by: beauty-bag <huwei169@huawei.com>

---
+++ interfaces/kits/napi/include/request_auth_from_user.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef FOUNDATION_RESOURCESCHEDULE_BACKGROUND_TASK_MGR_INTERFACES_KITS_NAPI_INCLUDE_REQUEST_AUTH_FROM_USER_H
+#define FOUNDATION_RESOURCESCHEDULE_BACKGROUND_TASK_MGR_INTERFACES_KITS_NAPI_INCLUDE_REQUEST_AUTH_FROM_USER_H
+
+#include <mutex>
+
+#include "common.h"
+#include "expired_callback.h"
+
+namespace OHOS {
+namespace BackgroundTaskMgr {
+class AuthCallbackInstance : public ExpiredCallback {
+public:
+    AuthCallbackInstance();
+
+    ~AuthCallbackInstance() override;
+
+    void OnExpired() override;
+
+    void OnExpiredAuth(int32_t authResult) override;
+
+    void SetCallbackInfo(const napi_env &env, const napi_ref &ref);
+
+private:
+    void DeleteNapiRef();
+    struct CallbackInfo {
+        napi_env env = nullptr;
+        napi_ref ref = nullptr;
+    };
+
+    CallbackInfo expiredCallbackInfo_;
+};
+
+extern std::map<int32_t, std::shared_ptr<ExpiredCallback>> authCallbackInstances_;
+extern std::mutex authCallbackLock_;
+
+napi_value RequestAuthFromUser(napi_env env, napi_callback_info info);
+}  // namespace BackgroundTaskMgr
+}  // namespace OHOS
+#endif  // FOUNDATION_RESOURCESCHEDULE_BACKGROUND_TASK_MGR_INTERFACES_KITS_NAPI_INCLUDE_REQUEST_AUTH_FROM_USER_H
