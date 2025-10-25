fb2de7ee28e62018b625fbcec50b2ef75eaccc25
beauty-bag <huwei169@huawei.com>
2025-10-25 14:44:02 +0800

    update: 更新文件 banner_notification_record.cpp
    Signed-off-by: beauty-bag <huwei169@huawei.com>

--- services/continuous_task/src/banner_notification_record.cpp
+++ services/continuous_task/src/banner_notification_record.cpp
@@ -14,6 +14,8 @@
  */
 
 #include "banner_notification_record.h"
+#include "common_utils.h"
+#include "continuous_task_log.h"
 
 namespace OHOS {
 namespace BackgroundTaskMgr {
@@ -100,6 +102,87 @@ void BannerNotificationRecord::SetUserId(int32_t userId)
 void BannerNotificationRecord::SetAppIndex(int32_t appIndex)
 {
     appIndex_ = appIndex;
+}
+
+void BannerNotificationRecord::SetRecordValue(const nlohmann::json &value)
+{
+    if (value.contains("bundleName") && value["bundleName"].is_string()) {
+        this->bundleName_ = value.at("bundleName").get<std::string>();
+    } else {
+        BGTASK_LOGE("no hava key: bundleName, or bunleName is not string type.");
+    }
+    if (value.contains("uid") && value["uid"].is_number_integer()) {
+        this->uid_ = value.at("uid").get<int32_t>();
+    } else {
+        BGTASK_LOGE("no hava key: uid, or uid is not number type.");
+    }
+    if (value.contains("notificationId") && value["notificationId"].is_number_integer()) {
+        this->notificationId_ = value.at("notificationId").get<int32_t>();
+    } else {
+        BGTASK_LOGE("no hava key: notificationId, or notificationId is not number type.");
+    }
+    if (value.contains("notificationLabel") && value["notificationLabel"].is_string()) {
+        this->notificationLabel_ = value.at("notificationLabel").get<std::string>();
+    } else {
+        BGTASK_LOGE("no hava key: notificationLabel, or notificationLabel is not string type.");
+    }
+    if (value.contains("appName") && value["appName"].is_string()) {
+        this->appName_ = value.at("appName").get<std::string>();
+    } else {
+        BGTASK_LOGE("no hava key: appName, or appName is not string type.");
+    }
+    if (value.contains("authResult") && value["authResult"].is_number_integer()) {
+        this->authResult_ = value.at("authResult").get<int32_t>();
+    } else {
+        BGTASK_LOGE("no hava key: authResult, or authResult is not number type.");
+    }
+    if (value.contains("userId") && value["userId"].is_number_integer()) {
+        this->userId_ = value.at("userId").get<int32_t>();
+    } else {
+        BGTASK_LOGE("no hava key: userId, or userId is not number type.");
+    }
+    if (value.contains("appIndex") && value["appIndex"].is_number_integer()) {
+        this->appIndex_ = value.at("appIndex").get<int32_t>();
+    } else {
+        BGTASK_LOGE("no hava key: appIndex, or appIndex is not number type.");
+    }
+}
+
+bool CheckRecodKey(const nlohmann::json &value)
+{
+    return !value["bundleName"].is_string() || !value["uid"].is_number_integer()
+        || !value["notificationId"].is_number_integer() || !value["notificationLabel"].is_string()
+        || !value["appName"].is_string() || !value["authResult"].is_number_integer() 
+        || !value["userId"].is_number_integer() || !value["appIndex"].is_number_integer();
+}
+
+bool BannerNotificationRecord::ParseFromJson(const nlohmann::json &value)
+{
+    if (value.is_null() || !value.is_object() || !CommonUtils::CheckJsonValue(value, { "bundleName",
+        "uid", "notificationId", "notificationLabel", "appName", "authResult", "userId", "appIndex"})) {
+        BGTASK_LOGE("bannerNotificationRecord no key.");
+        return false;
+    }
+    if (CheckRecodKey(value)) {
+        BGTASK_LOGE("bannerNotificationRecord parse from json fail.");
+        return false;
+    }
+    SetRecordValue(value);
+    return true;
+}
+
+std::string BannerNotificationRecord::ParseToJsonStr()
+{
+    nlohmann::json root;
+    root["bundleName"] = bundleName_;
+    root["uid"] = uid_;
+    root["notificationId"] = notificationId_;
+    root["notificationLabel"] = notificationLabel_;
+    root["appName"] = appName_;
+    root["authResult"] = authResult_;
+    root["userId"] = userId_;
+    root["appIndex"] = appIndex_;
+    return root.dump(CommonUtils::jsonFormat_);
 }
 }  // namespace BackgroundTaskMgr
 }  // namespace OHOS
