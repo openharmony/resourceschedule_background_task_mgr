554fecbd0ed7d5b1548d9674c8caa360b816a0af
beauty-bag <huwei169@huawei.com>
2026-02-06 10:25:11 +0800

    update: 更新文件 data_storage_helper.cpp
    Signed-off-by: beauty-bag <huwei169@huawei.com>

--- services/common/src/data_storage_helper.cpp
+++ services/common/src/data_storage_helper.cpp
@@ -45,7 +45,7 @@ const std::string ENABLE = "enable";
 const std::string DOZE_TIME = "doze_time";
 constexpr int32_t EXTENSION_SUCCESS_CODE = 0;
 constexpr int32_t EXTENSION_ERROR_CODE = 13500099;
-constexpr int32_t MAX_AUTH_RECORD_SIZE = 400 * 200;
+constexpr int32_t MAX_AUTH_RECORD_SIZE = 400 * 200; // 单个应用授权记录数据大小为400
 }
 
 DataStorageHelper::DataStorageHelper() {}
