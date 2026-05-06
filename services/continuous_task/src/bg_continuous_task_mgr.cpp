135b4e5ddefa2317ecb9d62d450c4a379c4f98a0
beauty-bag <huwei169@huawei.com>
2026-05-06 11:23:12 +0800

    包事件变化时，清除授权记录，更新判断条件
    Signed-off-by: beauty-bag <huwei169@huawei.com>

--- services/continuous_task/src/bg_continuous_task_mgr.cpp
+++ services/continuous_task/src/bg_continuous_task_mgr.cpp
@@ -2778,7 +2778,8 @@ void BgContinuousTaskMgr::OnContinuousTaskChanged(const std::shared_ptr<Continuo
     ReportHisysEvent(changeEventType, continuousTaskInfo);
 }
 
-void BgContinuousTaskMgr::OnBundleInfoChanged(const std::string &action, const std::string &bundleName, int32_t uid)
+void BgContinuousTaskMgr::OnBundleInfoChanged(const std::string &action, const std::string &bundleName, int32_t uid,
+    int32_t userId, int32_t appIndex)
 {
     if (!isSysReady_.load()) {
         BGTASK_LOGW("manager is not ready");
@@ -2793,7 +2794,8 @@ void BgContinuousTaskMgr::OnBundleInfoChanged(const std::string &action, const s
         cachedBundleInfos_.erase(uid);
         auto iterAuth = bannerNotificationRecord_.begin();
         while (iterAuth != bannerNotificationRecord_.end()) {
-            if (iterAuth->second->GetUid() != uid || iterAuth->second->GetBundleName() != bundleName) {
+            if (iterAuth->second->GetUserId() != userId || iterAuth->second->GetBundleName() != bundleName ||
+                iterAuth->second->GetAppIndex() != appIndex) {
                 iterAuth++;
                 continue;
             }
