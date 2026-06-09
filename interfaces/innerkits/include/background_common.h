16632e5189bb275f02f5fb88f6b4eb78a6a5ee9c
beauty-bag <huwei169@huawei.com>
2026-06-09 09:50:36 +0800

    update: 更新文件 background_common.h
    Signed-off-by: beauty-bag <huwei169@huawei.com>

--- interfaces/innerkits/include/background_common.h
+++ interfaces/innerkits/include/background_common.h
@@ -27,6 +27,16 @@ namespace {
     static constexpr int32_t API_VERSION_REQUEST_SPECIAL_USER_AUTH = 22;
     // 请求用户授权API 26接口
     static constexpr int32_t API_VERSION_REQUEST_SPECIAL_USER_AUTH_BY_DIALOG = 26;
+    // 长时任务自有hap包名
+    static constexpr char BUNDLE_NAME_CONTINUOUS_TASK_RESOURCE[] = "com.ohos.backgroundtaskmgr.resources";
+    // 特殊类型长时任务自定义授权弹窗Extension ability name
+    static constexpr char EXTENSION_ABILITY_NAME_SPECIAL_MODE_PERMISSION_DIALOG[] = "EnableBgTaskAuthDialog";
+    // 通过UEC拉起extension固定配置key
+    static constexpr char TYPE_KEY_START_UIEXTENSION[] = "ability.want.params.uiExtensionType";
+    // 特殊类型长时任务自定义授权弹窗-传参appIndex
+    static constexpr char PERMISSION_DIALOG_PARAM_APP_INDEX[] = "appIndex";
+    // 特殊类型长时任务自定义授权弹窗-传参taskMode（长时任务子类型）
+    static constexpr char PERMISSION_DIALOG_PARAM_SPECIAL_SUB_MODE[] = "taskMode";
 }
 }  // namespace BackgroundTaskMgr
 }  // namespace OHOS
