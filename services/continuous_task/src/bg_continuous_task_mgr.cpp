78ddc7b99a51bed101b7cef46ef1ea0f2c9b1258
beauty-bag <huwei169@huawei.com>
2026-09-18 20:29:51 +0800

    API 21更新/取消长时任务接口添加uid校验
    Signed-off-by: beauty-bag <huwei169@huawei.com>

--- services/continuous_task/src/bg_continuous_task_mgr.cpp
+++ services/continuous_task/src/bg_continuous_task_mgr.cpp
@@ -1200,7 +1200,8 @@ ErrCode BgContinuousTaskMgr::UpdateBackgroundRunningByTaskIdInner(int32_t uid,
         return ERR_BGTASK_CONTINUOUS_TASKID_INVALID;
     }
     auto findTask = [continuousTaskId](const auto &target) {
-        return continuousTaskId == target.second->continuousTaskId_ && target.second->isByRequestObject_;
+        return continuousTaskId == target.second->continuousTaskId_ && uid == target.second->uid_&&
+            target.second->isByRequestObject_;
     };
     auto findTaskIter = find_if(continuousTaskInfosMap_.begin(), continuousTaskInfosMap_.end(), findTask);
     if (findTaskIter == continuousTaskInfosMap_.end()) {
@@ -1904,7 +1905,7 @@ ErrCode BgContinuousTaskMgr::StopBackgroundRunningInner(int32_t uid, const std::
     if (continuousTaskId != -1) {
         // 新接口取消
         auto findTask = [continuousTaskId](const auto &target) {
-            return continuousTaskId == target.second->continuousTaskId_;
+            return continuousTaskId == target.second->continuousTaskId_ && uid == target.second->uid_;
         };
         auto findTaskIter = find_if(continuousTaskInfosMap_.begin(), continuousTaskInfosMap_.end(),
             findTask);
